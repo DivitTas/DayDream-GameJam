d56fe882e0867b5bc174ad865955eed0461e9d9c
DivitTas <divit.nt@gmail.com>
2025-09-27 11:39:48 +0530

    Added first person controller script

--- Assets/Scripts/FirstPersonCamera.cs
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class FirstPersonCamera : MonoBehaviour
 {
+    public Transform player;
+    private  float mouseSensitivity = 100f;
+    public float cameraPitch = 0f;
+    public float defaultFOV = 60f;
+    public Camera mainCamera;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.visible = true;
+        player = transform.parent;
+        Cursor.lockState = CursorLockMode.Locked;
+        mainCamera = GetComponentInChildren<Camera>();
+        mainCamera.fieldOfView = defaultFOV;
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        cameraPitch -= Input.GetAxis("Mouse Y") * mouseSensitivity * Time.fixedDeltaTime;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90f, 90f);
+        mainCamera.transform.localRotation = Quaternion.Euler(cameraPitch, 0f, 0f);
+        player.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivity * Time.fixedDeltaTime);
+
     }
 }
